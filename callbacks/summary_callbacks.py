72a878f4e564824417b5a9f2f0cf6ccdbc806c24
FelitoAguila <feloaguila@gmail.com>
2025-09-10 19:27:29 -0300

    Update summary_callbacks.py

--- callbacks/summary_callbacks.py
+++ callbacks/summary_callbacks.py
@@ -8,14 +8,29 @@ metrics = SubscriptionMetrics()
 
 def register_summary_callbacks(app):
     @app.callback(
+        Output('dollar-value', 'value'),
+        Input('get-dollar-value-button', 'n_clicks'),
+        prevent_initial_call=True  # Evita la llamada inicial sin valor
+    )
+    def fetch_dollar_value(n_clicks):
+        if n_clicks > 0:
+            try:
+                dolar_value = metrics.get_dolar_argentina()
+                return dolar_value
+            except Exception as e:
+                print(f"Error fetching dollar value: {e}")
+                return "No se pudo extraer el valor del dólar, ingrese manualmente"
+        return ""
+
+
+    @app.callback(
         Output('subs-summary', 'children'),
         Output('ingresos-summary', 'children'),
-        Input('dollar-value', 'value')
+        Input('dollar-value', 'value'),
+        prevent_initial_call=True  # Evita la llamada inicial sin valor
     )
     def update_summary_metrics(dolar_value):
-        if dolar_value == "No se pudo extraer el valor del dólar, ingrese manualmente":
-            dolar_value = 1200
-
+        
         # Métricas suscripciones
         active_tme_stripe_subs = metrics.get_tme_active_stripe_subs()
         active_tgo_stripe_subs = metrics.get_tgo_active_stripe_subs()
@@ -23,8 +38,8 @@ def register_summary_callbacks(app):
         total_active_subs = active_tme_stripe_subs + active_tgo_stripe_subs + authorized_mp_subs
 
         # Métricas de ingresos
-        ingresos_mp = round (metrics.get_mp_income(),2)
-        ingresos_stripe=round(metrics.get_stripe_income(),2)
+        ingresos_mp = round (metrics.get_last_month_mp_income(),2)
+        ingresos_stripe=round(metrics.get_last_month_stripe_income(),2)
         total_ingresos = round(ingresos_mp/dolar_value + ingresos_stripe, 2)
 
         return (
@@ -53,7 +68,7 @@ def register_summary_callbacks(app):
                 ], style={"marginBottom": "10px"}),
                 html.Div([
                     html.Span("Stripe TranscribeMe: ", style={"fontWeight": "bold"}),
-                    html.Span(f"{ingresos_stripe} USD (placeholder)")
+                    html.Span(f"{ingresos_stripe} USD")
                 ], style={"marginBottom": "5px"}),
                 html.Div([
                     html.Span("Mercado Pago: ", style={"fontWeight": "bold"}),
